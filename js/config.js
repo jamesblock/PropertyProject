5ba317b2703a4e0454e296fec5b72f4e2cfdcaee
Jamesblock <jfblock@me.com>
2021-05-23 11:16:31 +0100

    userpool setup

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-2_j9yovGs21', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1q2pdvpffqrqnsopusntf2ouoa', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
